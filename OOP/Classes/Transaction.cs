9a45fcb086a94f6c1ee584b407c46b93be92958e
Luis Raul Espinoza Barboza <luis.espinoza@buildbinder.com>
2018-04-20 10:46:25 -0600

    interfaces

---
+++ OOP/Classes/Transaction.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OOP.Interfaces;
+
+namespace OOP.Classes
+{
+	class Transaction : ITransactions
+	{
+		private Guid Code;
+		private double Amount;
+
+		public Transaction(double amount)
+		{
+			this.Code = Guid.NewGuid();
+			this.Amount = amount;
+		}
+
+		public double GetAmount()
+		{
+			return this.Amount;
+		}
+
+		public void Show()
+		{
+			Console.Write("Code: {0} \n Amount: ${1}", this.Code, this.Amount);
+		}
+	}
+}
---
+++ OOP/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OOP.Classes;
+
+namespace OOP
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Transaction transaction1 = new Transaction(225);
+			transaction1.Show();
+			Console.Write("\n Enter to exit");
+			Console.ReadLine();
+		}
+	}
+}
